71a607dcd864e4a39cd6f5fd22069d32628906b3
ColinTB <colin.turnerbloom@mail.utoronto.ca>
2015-06-21 17:11:04 -0400

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions cache the inverse of a matrix
+## makeCacheMatrix generates a list with a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse
+## get the value of the inverse
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinver <- function(inver) inv <<- inver
+  getinver <- function() inv
+  list(set = set, get = get,
+       setinver = setinver,
+       getinver = getinver)
 }
 
 
-## Write a short comment describing this function
+
+## CacheSolve gives the inverse. First it checks if the 
+## inverse already exists, if so it provides the inverse
+## if no then it calculates the inverse using setinver
+
+## *assumes the inverse exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinver()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinver(inv)
+  inv
 }
+
+#if you want to test it, create any matrix x
+## run m = makeCacheMatrix(x)
+## now use m$get() to get the matrix
+## cacheSolve(m) to solve it
+## run this last line again, and you should be prompted
+## that you are getting cached data, since the inverse
+## has already been solved for
+## TADA!
+
